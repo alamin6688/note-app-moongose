3c5b820c0ca70a3507fea1b63804fb69e4c07ab1
Alamin <alamin.kzs06@gmail.com>
2025-08-29 20:40:46 +0600

    Built-in and Custom validations [max/min] added

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -6,15 +6,27 @@ const userSchema = new Schema<IUser>({
     type: String,
     required: true,
     trim: true,
+    minLength: [3, "First Name must be at least 3 characters, got {VALUE}"],
+    maxLength: [10, "First Name must be at most 10 characters, got {VALUE}"],
   },
   lastName: {
     type: String,
     required: true,
     trim: true,
+    minLength: [3, "Last Name must be at least 3 characters, got {VALUE}"],
+    maxLength: [10, "Last Name must be at most 10 characters, got {VALUE}"],
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: [18, "Age must be at least 18, got {VALUE}"],
+    max: [60, "Age must be at most 60, got {VALUE}"],
   },
   email: {
     type: String,
+    unique: true,
     required: true,
+    lowercase: true,
     trim: true,
   },
   password: {
@@ -23,8 +35,9 @@ const userSchema = new Schema<IUser>({
   },
   role: {
     type: String,
-    enum: ["user", "admin"],
-    default: "user",
+    uppercase: true,
+    enum: ["USER", "ADMIN", "SUPERADMIN"],
+    default: "USER",
   },
 });
 
